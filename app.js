1da895ea3ec6473dee13ee55a5ca4da3dcf49b9c
tankez0r <76497823+tankez0r@users.noreply.github.com>
2024-02-03 19:40:47 -0300

    first commit
    ejercicio 2

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const port = 3030;
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(port, () => console.log("http://localhost:3030"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
